9d310f269604a35f6bfe84e902c01a86b9116891
Hauke Holtkamp <h.holtkamp@gmail.com>
2016-06-14 15:47:25 +0200

    Simple market list and reset button

--- market_list/models.py
+++ market_list/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+class MarketListEntry(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    position = models.IntegerField()
+
+
+class CompetitionRequest(models.Model):
+    FALLBACK_RULES = (
+        ('SH', 'Shift'),
+        ('WD', 'Withdraw')
+    )
+    name = models.CharField(max_length=200)
+    working_price = models.IntegerField()
+    target_position = models.IntegerField()
+    fallback_rule = models.CharField(max_length=1, choices=FALLBACK_RULES)
+    working_price_reduction_interval = models.FloatField()
+
+    def __str__(self):
+        return "{0}, {1} EUR, Pos {2}".format(self.name, self.working_price,
+                                              self.target_position)
+
+
+def fill_default_market_list():
+    MarketListEntry.objects.all().delete()
+
+    a = MarketListEntry(name="Aachener", price=44., position=0)
+    a.save()
+
+    b = MarketListEntry(name="Berliner", price=45., position=1)
+    b.save()
+
+    c = MarketListEntry(name="Chemnitzer", price=46., position=2)
+    c.save()
+
+    d = MarketListEntry(name="Dortmunder", price=47., position=3)
+    d.save()
--- market_list/views.py
+++ market_list/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import MarketListEntry, fill_default_market_list
+
+
+def index(request):
+    list = MarketListEntry.objects.order_by('position')
+    context = {'list': list}
+    return render(request, 'market_list/index.html', context)
+
+
+def fill_market_list(request):
+    fill_default_market_list()
+    return index(request)
+
